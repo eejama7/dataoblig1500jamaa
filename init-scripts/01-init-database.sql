e7a32de08f0410b2ed523919d2a595722e6084c6
Jama Abdirashid <eejama@MacBookAir.home>
2026-03-01 22:53:45 +0100

    jeg leverer blankt

---
+++ init-scripts/01-init-database.sql
@@ -0,0 +1,23 @@
+-- ============================================================================
+-- DATA1500 - Oblig 1: Arbeidskrav I våren 2026
+-- Initialiserings-skript for PostgreSQL
+-- ============================================================================
+
+-- Opprett grunnleggende tabeller
+
+
+
+-- Sett inn testdata
+
+
+
+-- DBA setninger (rolle: kunde, bruker: kunde_1)
+
+
+
+-- Eventuelt: Opprett indekser for ytelse
+
+
+
+-- Vis at initialisering er fullført (kan se i loggen fra "docker-compose log"
+SELECT 'Database initialisert!' as status;
---
+++ test-scripts/queries.sql
@@ -0,0 +1,8 @@
+-- ============================================================================
+-- TEST-SKRIPT FOR OBLIG 1
+-- ============================================================================
+
+-- Kjør med: docker-compose exec postgres psql -h -U admin -d data1500_db -f test-scripts/queries.sql
+
+-- En test med en SQL-spørring mot metadata i PostgreSQL (kan slettes fra din script)
+select nspname as schema_name from pg_catalog.pg_namespace;
